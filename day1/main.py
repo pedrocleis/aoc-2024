e948dd6de6c1fd0e6cacdcf1f0fb8a7337285f5e
Pedro Cleis <pedrokleiz@gmail.com>
2024-12-02 16:56:14 -0300

    make part1 into a function

--- day1/main.py
+++ day1/main.py
@@ -3,18 +3,20 @@ locations = f.read().split("\n")
 left_locations = []
 right_locations = []
 
+def part1(locations ):
+  for location in locations:
+    split_location = (location.split("   "))
+    left_locations.append(split_location[0])
+    right_locations.append(split_location[1])
 
-for location in locations:
-  split_location = (location.split("   "))
-  left_locations.append(split_location[0])
-  right_locations.append(split_location[1])
+  left_locations.sort()
+  right_locations.sort()
 
-left_locations.sort()
-right_locations.sort()
+  total_distance = 0
 
-total_distance = 0
+  for i in range(len(left_locations)):
+    total_distance += abs(int(left_locations[i]) - int(right_locations[i]))
 
-for i in range(len(left_locations)):
-  total_distance += abs(int(left_locations[i]) - int(right_locations[i]))
+  return(total_distance)
 
-print(total_distance)
+print(part1(locations))
