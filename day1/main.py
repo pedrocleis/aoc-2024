183806a9ada36a21d342a9a6ab3d976d40ff3e5f
Pedro Cleis <pedrokleiz@gmail.com>
2024-12-02 16:51:40 -0300

    part1 day1

---
+++ day1/main.py
@@ -0,0 +1,20 @@
+f = open("inputs/day1.txt", "r", encoding="utf-8")
+locations = f.read().split("\n")
+left_locations = []
+right_locations = []
+
+
+for location in locations:
+  split_location = (location.split("   "))
+  left_locations.append(split_location[0])
+  right_locations.append(split_location[1])
+
+left_locations.sort()
+right_locations.sort()
+
+total_distance = 0
+
+for i in range(len(left_locations)):
+  total_distance += abs(int(left_locations[i]) - int(right_locations[i]))
+
+print(total_distance)
